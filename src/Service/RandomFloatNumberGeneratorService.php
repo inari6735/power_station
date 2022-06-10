b71a113615f9067d8d8fc9cb822bd9b51d5ecd3b
inari6735 <32057515+inari6735@users.noreply.github.com>
2022-06-10 02:27:52 +0200

    fix: change default unit

--- src/Service/RandomFloatNumberGeneratorService.php
+++ src/Service/RandomFloatNumberGeneratorService.php
@@ -5,6 +5,6 @@ namespace App\Service;
 class RandomFloatNumberGeneratorService
 {
     public function getRandomFloatNumber(): float {
-        return mt_rand(0, 10000) / 10000;
+        return mt_rand(0, 10000000000) / 10000000;
     }
 }
